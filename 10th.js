cf84abb94712db5e1338f4642cca521ba89f25ae
Robel <155951971+Robiin28@users.noreply.github.com>
2024-06-25 16:20:51 +0300

    cleaning unwanted javascript files

--- 10th.js
+++
@@ -1,6 +0,0 @@
-Server.on('request',(req,res)=>{
-    let rs=fs.createReadStream('./');
-    rs.pipe(res);
-})
-//pipe method only can b used in readable stream and can shorten long source code
-// readableSource.pipe(writableDestination);
